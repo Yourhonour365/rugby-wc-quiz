06d121971fb7b24b7762c7b7ca2035561098c531
Yourhonour365 <jed@commerxia.io>
2025-07-30 17:08:23 +0100

    format legends images. add js to stop accordion opening at end of accordion on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
-console.log("✅ JS file is linked correctly!");
 
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    const accordion = document.getElementById('knowledgeAccordion');
+
+    accordion.addEventListener('shown.bs.collapse', function (event) {
+      const header = event.target.closest('.accordion-item');
+      if (header) {
+        header.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    });
+  });
+</script>
